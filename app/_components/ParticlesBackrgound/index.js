a266e4e0f85a85e7e27d6d0f936b1b5dfe0e1ba1
Asami <asa2_pokecho@icloud.com>
2024-11-16 17:17:33 +0900

    ten

--- app/_components/ParticlesBackrgound/index.js
+++ app/_components/ParticlesBackrgound/index.js
@@ -2,11 +2,8 @@
 import React, { useCallback } from 'react'
 import Particles from "react-tsparticles";
 import { loadSlim } from "tsparticles-slim";
-import template from "@/public/assets/particlesjs-config.json";
 
 export default function ParticlesBackrgound({ children }) {
-
-    const params = template;
 
     const particlesInit = useCallback(async engine => {
         console.log(engine);
@@ -24,7 +21,120 @@ export default function ParticlesBackrgound({ children }) {
                 id="tsparticles"
                 init={particlesInit}
                 loaded={particlesLoaded}
-                options={params}
+                options={{
+                    fullScreen: {
+                      enable: true,
+                      zIndex: 100
+                    },
+                    particles: {
+                      number: {
+                        value: 70,
+                        density: {
+                          enable: true,
+                          value_area: 800
+                        }
+                      },
+                      color: {
+                        value: "#fff"
+                      },
+                      shape: {
+                        type: "circle",
+                        stroke: {
+                          width: 0,
+                          color: "#000000"
+                        },
+                        polygon: {
+                          nb_sides: 5
+                        },
+                        image: {
+                          src: "img/github.svg",
+                          width: 100,
+                          height: 100
+                        }
+                      },
+                      opacity: {
+                        value: 0.8,
+                        random: true,
+                        anim: {
+                          enable: false,
+                          speed: 1,
+                          opacity_min: 0.1,
+                          sync: false
+                        }
+                      },
+                      size: {
+                        value: 10,
+                        random: true,
+                        anim: {
+                          enable: false,
+                          speed: 40,
+                          size_min: 0.1,
+                          sync: false
+                        }
+                      },
+                      line_linked: {
+                        enable: false,
+                        distance: 500,
+                        color: "#ffffff",
+                        opacity: 0.4,
+                        width: 2
+                      },
+                      move: {
+                        enable: true,
+                        speed: 1.5,
+                        direction: "bottom",
+                        random: false,
+                        straight: false,
+                        out_mode: "out",
+                        bounce: false,
+                        attract: {
+                          enable: false,
+                          rotateX: 600,
+                          rotateY: 1200
+                        }
+                      }
+                    },
+                    interactivity: {
+                      detect_on: "canvas",
+                      events: {
+                        onhover: {
+                          enable: true,
+                          mode: "bubble"
+                        },
+                        onclick: {
+                          enable: true,
+                          mode: "repulse"
+                        },
+                        resize: true
+                      },
+                      modes: {
+                        grab: {
+                          distance: 400,
+                          line_linked: {
+                            opacity: 0.5
+                          }
+                        },
+                        bubble: {
+                          distance: 400,
+                          size: 4,
+                          duration: 0.3,
+                          opacity: 1,
+                          speed: 3
+                        },
+                        repulse: {
+                          distance: 200,
+                          duration: 0.4
+                        },
+                        push: {
+                          particles_nb: 4
+                        },
+                        remove: {
+                          particles_nb: 2
+                        }
+                      }
+                    },
+                    retina_detect: true
+                  }}
             />
             {children}
         </>
